8a3f0a12616d3f1eb28cab8b40cc3e4b1175a540
Ricardo <rick_ferry1@hotmail.com>
2024-08-02 01:10:42 -0300

    Adjust

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { getRandomPokemons } from '../api/pokemonApi';
+import { Link } from 'react-router-dom';
 
 const PokemonList = () => {
   const [pokemons, setPokemons] = useState([]);
