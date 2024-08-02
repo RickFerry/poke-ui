896fb4254d23920d180017ec0dc972578f35d52b
Ricardo <rick_ferry1@hotmail.com>
2024-08-02 00:11:39 -0300

    Ajusting

--- src/api/pokemonApi.js
+++ src/api/pokemonApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: ' http://localhost:5219/api', // Certifique-se de que a URL e porta estejam corretas
+  baseURL: ' http://localhost:5219/api',
 });
 
 export const getRandomPokemons = () => api.get('/pokemon/random');
